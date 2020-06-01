9857a7b58570fa9e401c977f09f50d0ab690c62a
Bappaditya <bappaditya.sasmal@qlikchain.com>
2020-06-01 21:53:16 +0530

    Debug messages added

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,12 +1,16 @@
+const debug = require("debug")("be-harvester:Migration");
+
 import {DB_TYPE, DB_URL, REDIS_HOSTS, REDIS_PORTS, Store, TTL} from "./config";
 
 Store.DataStore(DB_TYPE, DB_URL, REDIS_HOSTS, REDIS_PORTS, TTL)
 .then(async (store) => {
     await store.migration.up();
     console.log("Migration Success");
+    debug("Migration Success");
     process.exit();
 })
 .catch(error => {
     console.error("Migration Failed", error);
+    debug("Migration Failed", error);
     process.exit();
 });
--- src/rollback.ts
+++ src/rollback.ts
@@ -1,12 +1,16 @@
+const debug = require("debug")("be-harvester:Rollback");
+
 import {DB_TYPE, DB_URL, REDIS_HOSTS, REDIS_PORTS, Store, TTL} from "./config";
 
 Store.DataStore(DB_TYPE, DB_URL, REDIS_HOSTS, REDIS_PORTS, TTL)
 .then(async (store) => {
     await store.migration.down();
     console.log("Rollback Success");
+    debug("Rollback Success");
     process.exit();
 })
 .catch(error => {
     console.error("Rollback Failed", error);
+    debug("Rollback Failed", error);
     process.exit();
 });
