3bcfce814687813291175e02b6bcd7b23fad1918
Bopsi <bshangout@gmail.com>
2020-10-17 23:47:11 +0530

    hex type check added to audits

--- src/audit/index.ts
+++ src/audit/index.ts
@@ -69,11 +69,11 @@ export default class Audit {
         if (args && Array.isArray(args)) {
             args.forEach(obj => {
                 // debug(obj);
-                let name = hexToString(obj.name);
+                let name = isHex(obj.name)?hexToString(obj.name):obj.name;
                 let fact = null;
 
                 if (obj.fact.Text) {
-                    fact = hexToString(obj.fact.Text);
+                    fact = isHex(obj.fact.Text)?hexToString(obj.fact.Text):obj.fact.Text;
                 } else if (obj.fact.Bool !== null) {
                     fact = obj.fact.Bool;
                 } else if (obj.fact.U8) {
